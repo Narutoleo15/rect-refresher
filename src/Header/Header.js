d00b1c60feb17c61230c8b5a99a372f562e4ba49
Ricky Pickens <narutoleo15@yahoo.com>
2022-09-16 01:33:02 -0500

    fix

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './Header.css'
 export default function Header() {
     return (
-        <section id="header center">
-            <Header>
+        <section id="header">
+            <Header class="center">
                 <h1>Todo-List</h1>
             </Header>
         </section>
